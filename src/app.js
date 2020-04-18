1e4e2ea0a94e39594ef3fff0d4487d031ac86684
Brendenson Andrade <brendensond@gmail.com>
2020-04-18 02:49:10 -0300

    Desafio finalizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,59 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const repository = { id: uuid(), ...request.body, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const indice = repositories.findIndex((elem) => elem.id === id);
+
+  if (indice === -1) return response.sendStatus(400);
+
+  const { title, url, techs } = request.body;
+
+  const repository = repositories.find(elem => elem.id === id);
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  repositories[indice] = repository;
+  
+  return response.status(200).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const indice = repositories.findIndex((elem) => elem.id === id);
+
+  if (indice === -1) return response.sendStatus(400);
+
+  repositories.splice(indice, 1);
+
+  return response.sendStatus(204);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find((elem) => elem.id === id);
+
+  if (!repository) return response.sendStatus(400);
+
+  repository.likes += 1;
+
+  return response.status(201).json(repository);
 });
 
 module.exports = app;
